a38e166a966261f7a22c038f5f7e4a389b9256ef
Pooya Golchian <pooya.golchian@gmail.com>
2026-02-27 23:53:08 +0400

    feat(docs): remove NavThemeSwitcher from nav bar

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,22 +1,13 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import { h } from 'vue'
 import { PRESET_THEMES, VueMultipleThemesPlugin } from 'vue-multiple-themes'
 import ColorGrid from '../components/ColorGrid.vue'
 import LiveDemo from '../components/LiveDemo.vue'
-import NavThemeSwitcher from '../components/NavThemeSwitcher.vue'
 import ThemeDemo from '../components/ThemeDemo.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
-
-  // Inject NavThemeSwitcher into the nav bar (replaces the dark/light toggle)
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'nav-bar-content-after': () => h(NavThemeSwitcher),
-    })
-  },
 
   enhanceApp({ app }) {
     // Install the theme plugin so demos work inside docs
